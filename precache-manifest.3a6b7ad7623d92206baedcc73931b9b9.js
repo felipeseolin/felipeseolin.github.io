e20232b5e6e5225fb6b0646e319d398ae7604c0f
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-09-26 17:59:26 -0300

    Updates

---
+++ precache-manifest.3a6b7ad7623d92206baedcc73931b9b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e43e2edbd91fb70f1c0c03fb1663264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18ffa835f9104269d055",
+    "url": "/static/css/2.e86a0a7a.chunk.css"
+  },
+  {
+    "revision": "a7df611d3375293f62aa",
+    "url": "/static/css/main.355adad5.chunk.css"
+  },
+  {
+    "revision": "18ffa835f9104269d055",
+    "url": "/static/js/2.e100dec7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e100dec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7df611d3375293f62aa",
+    "url": "/static/js/main.952655c5.chunk.js"
+  },
+  {
+    "revision": "61dd38e3f743c2fb5a63",
+    "url": "/static/js/runtime-main.15ef25dd.js"
+  }
+]);
