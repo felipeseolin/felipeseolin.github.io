499defcd51e913adba5d3259c71870be02eaa893
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-09-27 14:22:41 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a89ee1242d5baea42e44b28891d06d1.js"
+  "/precache-manifest.0a808d4549eb354361fa1f14db2f3fa4.js"
 );
 
 self.addEventListener('message', (event) => {
