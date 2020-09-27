0643bbb83e6bfc6edbc746edb221a71300633bca
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-09-26 23:33:58 -0300

    Updates

---
+++ precache-manifest.183bd6f2c9ac843bf87210bf754badbe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b14573eb0ec66394481c5ddc4f3f049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e253a57057a3ce9aa3a0",
+    "url": "/static/css/2.a5ed57c3.chunk.css"
+  },
+  {
+    "revision": "0042321290c8109e42e0",
+    "url": "/static/css/main.ad40478a.chunk.css"
+  },
+  {
+    "revision": "e253a57057a3ce9aa3a0",
+    "url": "/static/js/2.b466fcae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b466fcae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0042321290c8109e42e0",
+    "url": "/static/js/main.9bb9d690.chunk.js"
+  },
+  {
+    "revision": "61dd38e3f743c2fb5a63",
+    "url": "/static/js/runtime-main.15ef25dd.js"
+  },
+  {
+    "revision": "82207fd66f213d83f948c99ecb6fe6eb",
+    "url": "/static/media/avatar.82207fd6.png"
+  },
+  {
+    "revision": "9fec003025d1e2b2fc2d9972bf707ce5",
+    "url": "/static/media/iconHeader.9fec0030.png"
+  },
+  {
+    "revision": "91130ddd30c7d48162828941eeea63b5",
+    "url": "/static/media/oldPortfolio.91130ddd.png"
+  },
+  {
+    "revision": "d976a9965024dab0ac3f51a76da8fa3d",
+    "url": "/static/media/tvUtfpr.d976a996.png"
+  }
+]);
